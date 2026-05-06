f0149b539a0d1d96ae01e76c1c623a7521c98668
gnayak21 <77711026+gnayak21@users.noreply.github.com>
2026-05-06 17:16:12 -0500

    supabase config 2nd try

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,9 @@
-export const SUPABASE_URL = "https://imbousgfqgdtssmtryso.supabase.com";
+export const SUPABASE_URL = "https://imbousgfqgdtssmtryso.supabase.co";
 export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_iOVILGjFGNa0yEqFk0qhTw_taugOYbZ";
 
 
 // sb_publishable_iOVILGjFGNa0yEqFk0qhTw_taugOYbZ
+// https://imbousgfqgdtssmtryso.supabase.co
 
 
 
